ba6e75da8666e1cfb2529691a0c03c225e788701
Iván Sánchez <isancheznetwork@gmail.com>
2019-03-29 17:07:38 +0100

    feat(api): External mongodb and compilations process

---
+++ api/build/environment/config.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+ * Basic configuration replace these declarations with environment variables on separated files
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    mongodb: {
+        uri: 'mongodb://localhost/webpush',
+        uriExternal: 'mongodb+srv://admin:Admin123@cluster0-68gmt.mongodb.net/webpush?retryWrites=true',
+    }
+};
+//# sourceMappingURL=config.js.map
